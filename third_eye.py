f03746c8f737464e5470a4a41c2bfafdd7724e59
abysmo <eterniumsoul@gmail.com>
2021-03-15 02:09:17 +0200

    third_eye: add interrupt handler
    SIGINT handler added to stop and
    resume script.

--- third_eye.py
+++ third_eye.py
@@ -9,6 +9,8 @@ from PIL import Image
 from time import sleep
 import cv2
 import playsound
+import signal
+import sys
 
 pattern = [0xff,0xff,0xff,0xff,0xff,
            0x00,0x00,0x00,0x00,0x00,
@@ -25,24 +27,34 @@ y1 = 675
 y2 = 1000
 
 #Images PATH
-img_path_out_raw = 'sc.png'
-img_path_out = 'img_processed.bmp'
+img_out_raw = 'sc.png'
+img_out = 'img_processed.bmp'
+alarm_sound = 'alarm2.mp3'
+
+def sigint_handler(sig, frame):
+    try:
+        i = input("Execution suspended, press Enter to resume or Ctrl+D to exit")
+    except:
+        sys.exit()
+    print("Resumed.")
+
+signal.signal(signal.SIGINT, sigint_handler)
 print("Third eye online")
 while(True):
     cycle_time = 1
     image = ImageGrab.grab(bbox=(x1,y1,x2,y2))
-    image.save(img_path_out_raw)
+    image.save(img_out_raw)
 
-    image = cv2.imread(img_path_out_raw, cv2.IMREAD_GRAYSCALE)
+    image = cv2.imread(img_out_raw, cv2.IMREAD_GRAYSCALE)
     ret,image = cv2.threshold(image, 230, 255, cv2.THRESH_BINARY)
-    cv2.imwrite(img_path_out,image)
+    cv2.imwrite(img_out,image)
 
-    in_file = open(img_path_out, "rb") # opening for [r]eading as [b]inary
+    in_file = open(img_out, "rb") # opening for [r]eading as [b]inary
     data = in_file.read()
     in_file.close()
 
     if b_pattern in data:
         print("Neut found!")
-        playsound.playsound('alarm2.mp3')
+        playsound.playsound(alarm_sound)
         cycle_time = 5
     sleep(cycle_time)
