8ca1a6581a7e42c7c7cc5e218a78c4e060c57bd9
abysmo <eterniumsoul@gmail.com>
2021-03-18 23:47:58 +0200

    third_eye.py: add boost local check
    Added boost local feature.
    Added few notification audiofiles.

--- third_eye.py
+++ third_eye.py
@@ -6,8 +6,8 @@ pip3 install playsound
 
 from PIL import ImageGrab
 from PIL import Image
-from time import sleep
 import numpy
+import copy
 import time
 import io
 import cv2
@@ -23,17 +23,22 @@ pattern = [0xff,0xff,0xff,0xff,0xff,
            0xff,0xff,0xff,0xff]
 b_pattern = bytes(pattern)
 
-#screenshot params
+#screenshot params (x1-x2 diff should be == 10px)
 x1 = 692
 x2 = 702
 y1 = 675
 y2 = 1000
 
 #variables
-save_images = False
+header_offset = 0x435
 img_out_raw = 'img_raw.png'
 img_out = 'img_processed.bmp'
 alarm_sound = 'alarm2.mp3'
+notification_sound = "notification2.mp3"
+bmp_image_buffer_prev = bytes()
+#debug settings
+save_images = False
+save_img_bin_dump = False
 
 
 def sigint_handler(sig, frame):
@@ -42,6 +47,21 @@ def sigint_handler(sig, frame):
     except:
         sys.exit()
     print("Resumed.")
+
+def check_local_boost(bin_array, bin_array_prev):
+    val1 = bin_array.count(0xff)
+    val2 = bin_array_prev.count(0xff)
+    if val1 > val2:
+        print(time.strftime("[%H:%M:%S]") + "Local boosted")
+        playsound.playsound(notification_sound)
+    bin_array_prev = bin_array
+    return bin_array_prev
+
+def check_neutrals(bin_array):
+    if b_pattern in bin_array:
+            print(time.strftime("[%H:%M:%S]") + "Neut found!")
+            playsound.playsound(alarm_sound)
+            cycle_time = 7
 
 signal.signal(signal.SIGINT, sigint_handler)
 print("Third eye online. Ctrl+C for suspend.")
@@ -58,10 +78,16 @@ while(True):
         cv2.imwrite(img_out,image)
 
     ret,buffer = cv2.imencode(".bmp", image)
-    bmp_image_buffer = io.BytesIO(buffer).read()
+    bmp_image_buffer = io.BytesIO(buffer)
+    bmp_image_buffer.flush()
+    bmp_image_buffer.seek(header_offset)
+    bmp_image_buffer = bmp_image_buffer.read()
+    
+    if save_img_bin_dump:
+        with open('img_dump', 'w') as file:
+            file.write(str(bmp_image_buffer))
 
-    if b_pattern in bmp_image_buffer:
-        print(time.strftime("[%H:%M:%S]") + "Neut found!")
-        playsound.playsound(alarm_sound)
-        cycle_time = 5
-    sleep(cycle_time)
+    bmp_image_buffer_prev = check_local_boost(bmp_image_buffer,bmp_image_buffer_prev)
+    check_neutrals(bmp_image_buffer)
+    
+    time.sleep(cycle_time)
