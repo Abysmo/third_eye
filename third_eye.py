0a5152fa174e1048badb6986720cd5a6c20b9b90
abysmo <eterniumsoul@gmail.com>
2021-03-22 01:35:24 +0200

    Rework screeshot capture. Add Win32Api lib
    Screenshow now made only when eve window is active
    You can choose eve window for monitoring
    WinApi library added to make work with eve window
    more convenient.

--- third_eye.py
+++ third_eye.py
@@ -1,8 +1,11 @@
+# Mulithreading doesn't work propertly inside python interpreter
+# because input() call freezes all the threads unlike terminal.
+# in windows terminal(console) all works fine.
 """
 pip3 install pillow
 pip3 install opencv-python
 pip3 install playsound
-pip3 install keyboard
+pip3 install win32gui
 """
 
 from PIL import ImageGrab
@@ -17,6 +20,7 @@ import playsound
 import signal
 import sys
 import threading
+import win32gui
 
 #match as "=" in 10xN px. screenshot
 pattern = [0xff,0xff,0xff,0xff,0xff,
@@ -43,13 +47,13 @@ settings = {
 'alarm_repeat' : True,
 'alarm_repeat_time' : 5,
 'image_ref_rate' : 1,
-#debug settings
-'save_images' : False,
-'save_img_bin_dump' : False,
-#vars
 'bmp_image_buffer_prev' : bytes(),
 'bmp_image_buffer' : bytes(),
 'global_lock' : threading.Lock(),
+'eve_win_hwnd' : int(),
+#debug settings
+'save_images' : False,
+'save_img_bin_dump' : False
 }
 
 #functions
@@ -83,6 +87,14 @@ def image_capture():
     global settings
     while(True):
         settings['global_lock'].acquire()
+        #check is eve window are active, if not - wait.
+        while True:
+            if settings['eve_win_hwnd'] != win32gui.GetForegroundWindow():
+                time.sleep(1)
+                continue
+            else:
+                break
+
         image = ImageGrab.grab(bbox=(x1,y1,x2,y2))
 
         image = cv2.cvtColor(numpy.array(image), cv2.COLOR_BGR2GRAY)
@@ -103,27 +115,33 @@ def image_capture():
             with open('img_dump', 'w') as file:
                 file.write(str(settings['bmp_image_buffer']))
         settings['global_lock'].release()
+        #print(time.strftime("[%H:%M:%S]") + "img captured") # Debug line
         time.sleep(settings['image_ref_rate'])
 
-def print_status():
+def print_variables():
+    global settings
     print(f"\
-settings[is_running] = {settings[is_running] }\n\
-settings[alarm_repeat] = {settings[alarm_repeat]}\n\
-settings[alarm_repeat_time] = {settings[alarm_repeat_time]}\n\
-settings[image_ref_rate] = {settings[image_ref_rate]}\
+settings[is_running] = {settings['is_running']}\n\
+settings[alarm_repeat] = {settings['alarm_repeat']}\n\
+settings[alarm_repeat_time] = {settings['alarm_repeat_time']}\n\
+settings[image_ref_rate] = {settings['image_ref_rate']}\n\
+settings[eve_win_hwnd] = {settings['eve_win_hwnd']} : {win32gui.GetWindowText(settings['eve_win_hwnd'])}\n\
 ")
 
+#TODO: make alarm repeat and time
 def print_usage():
     print("\
 s - start\stop script\n\
+w - change eve window\n\
 u - print usage\n\
+v - print settings\n\
 r[xx] - set alarm repeat (0 - repeat off)\n\
 E - exit script\n\
 ")
 
 def command_processing():
     global settings
-    print("Third eye online.")
+    print(time.strftime("[%H:%M:%S]") + "Third eye online.")
     print_usage()
     while True:
         cmd = input()
@@ -138,14 +156,37 @@ def command_processing():
                 print("Script resumed")
         elif cmd[0] == 'u':
             print_usage()
+        elif cmd[0] == 'v':
+            print_variables()
         elif cmd[0] == 'E':
             os._exit(0)
+        elif cmd[0] == 'w':
+            select_eve_window()
         else:
             print("Unknown cmd")
 
+def get_eve_win_list( hwnd, win_list ):
+    if ('EVE -' in win32gui.GetWindowText(hwnd)) and (hwnd not in win_list):
+        win_list.append(hwnd)
+
+def select_eve_window():
+    global settings
+    eve_win_list = list()
+    settings['global_lock'].acquire()
+    while True:
+        win32gui.EnumWindows(get_eve_win_list, eve_win_list)
+        if len(eve_win_list):
+            for i in eve_win_list:
+                print(f"[{eve_win_list.index(i)}] : [{win32gui.GetWindowText(i)}]")
+        win_num = input("select window or hit Enter to refresh: ")
+        if win_num:
+            settings['eve_win_hwnd'] = eve_win_list[int(win_num[0])]
+            settings['global_lock'].release()
+            return
+
 
 ###############-MAIN-###############
-            
+
 #init threads
 threads = {
 'cn' : threading.Thread(target=check_neutrals),
@@ -153,6 +194,9 @@ threads = {
 'ic' : threading.Thread(target=image_capture),
 'cp' : threading.Thread(target=command_processing)
 }
+
+select_eve_window()
+
 #start threads
 threads['cn'].start()
 threads['cb'].start()
@@ -161,4 +205,3 @@ threads['cp'].start()
 
 while True:
     time.sleep(1)
-
