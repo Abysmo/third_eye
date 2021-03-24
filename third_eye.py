7a939dd1744a4f85775625c4d2e7ae4ab7b0f5e2
abysmo <eterniumsoul@gmail.com>
2021-03-24 21:29:55 +0200

    Add alarm repeat settings and feature.
    Alarm repeat settings added.
    Alarm can be silenced after first sound signal
    when local boosted by neut.
    Sound lock added. Sometimes sound crashes when
    two threads use sound lib simultaneously.
    Added Highlight zone for screenshot zone select
    feature. It will be implemented in next few commits.
    Moved sound files in subfolder.

--- third_eye.py
+++ third_eye.py
@@ -31,18 +31,21 @@ pattern = [0xff,0xff,0xff,0xff,0xff,
            0xff,0xff,0xff,0xff]
 b_pattern = bytes(pattern)
 
+screenshot_params = {
 #screenshot params (x1-x2 diff should be == 10px)
-x1 = 692
-x2 = 702
-y1 = 675
-y2 = 1000
+'scr_x1' : int(),
+'scr_x2' : int(),
+'scr_y1' : int(),
+'scr_y2' : int()
+}
 
 settings = {
+#settings
 'header_offset' : 0x435,
 'img_out_raw' : 'img_raw.png',
 'img_out' : 'img_processed.bmp',
-'alarm_sound' : 'alarm2.mp3',
-'notification_sound' : "notification2.mp3",
+'alarm_sound' : 'audio/alarm2.mp3',
+'notification_sound' : 'audio/notification2.mp3',
 'is_running' : True,
 'alarm_repeat' : True,
 'alarm_repeat_time' : 5,
@@ -50,6 +53,7 @@ settings = {
 'bmp_image_buffer_prev' : bytes(),
 'bmp_image_buffer' : bytes(),
 'global_lock' : threading.Lock(),
+'sound_lock' : threading.Lock(),
 'eve_win_hwnd' : int(),
 #debug settings
 'save_images' : False,
@@ -57,8 +61,7 @@ settings = {
 }
 
 #functions
-def check_local_boost():
-    global settings
+def check_local_boost(settings):
     while True:
         settings['global_lock'].acquire()
         val1 = settings['bmp_image_buffer'].count(0xff)
@@ -66,60 +69,76 @@ def check_local_boost():
         settings['global_lock'].release()
         if val1 > val2:
             print(time.strftime("[%H:%M:%S]") + "Local boosted")
+            settings['sound_lock'].acquire()
             playsound.playsound(settings['notification_sound'])
+            settings['sound_lock'].release()
         settings['bmp_image_buffer_prev'] = settings['bmp_image_buffer']
         time.sleep(1)
 
-def check_neutrals():
-    global settings
+def check_neutrals(settings):
+    is_alarmed = False
     while True:
         settings['global_lock'].acquire()
         pos = settings['bmp_image_buffer'].find(b_pattern)
         settings['global_lock'].release()
-        if pos != -1:
-            print(time.strftime("[%H:%M:%S]") + "Neut found!")
+        if pos != -1 and settings['alarm_repeat_time'] != 0:
+            if is_alarmed == False: #only for print
+                print(time.strftime("[%H:%M:%S]") + "Neut found!")
+            settings['sound_lock'].acquire()
             playsound.playsound(settings['alarm_sound'])
+            settings['sound_lock'].release()
+            is_alarmed = True
             time.sleep(settings['alarm_repeat_time'])
             continue
+        elif pos != -1 and pos and settings['alarm_repeat_time'] == 0:
+            if is_alarmed == False:
+                print(time.strftime("[%H:%M:%S]") + "Neut found!")
+                settings['sound_lock'].acquire()
+                playsound.playsound(settings['alarm_sound'])
+                settings['sound_lock'].release()
+                is_alarmed = True
+            time.sleep(1)
+            continue
+        else:
+            is_alarmed = False
+
         time.sleep(1)
 
-def image_capture():
-    global settings
+def image_capture(settings):
     while(True):
-        settings['global_lock'].acquire()
+        settings['bmp_image_buffer'] = b'0x00'
         #check is eve window are active, if not - wait.
-        while True:
-            if settings['eve_win_hwnd'] != win32gui.GetForegroundWindow():
-                time.sleep(1)
-                continue
-            else:
-                break
+        if settings['eve_win_hwnd'] == win32gui.GetForegroundWindow():
+            settings['global_lock'].acquire()
+            image = ImageGrab.grab(bbox=(\
+            screenshot_params['scr_x1'], \
+            screenshot_params['scr_y1'], \
+            screenshot_params['scr_x2'], \
+            screenshot_params['scr_y2']))
 
-        image = ImageGrab.grab(bbox=(x1,y1,x2,y2))
+            image = cv2.cvtColor(numpy.array(image), cv2.COLOR_BGR2GRAY)
+            if settings['save_images']: #save image if enabled
+                cv2.imwrite(settings['img_out_raw'],image) 
+            
+            ret,image = cv2.threshold(image, 230, 255, cv2.THRESH_BINARY)
+            if settings['save_images']: #save image if enabled
+                cv2.imwrite(settings['img_out'],image)
 
-        image = cv2.cvtColor(numpy.array(image), cv2.COLOR_BGR2GRAY)
-        if settings['save_images']: #save image if enabled
-            cv2.imwrite(settings['img_out_raw'],image) 
+            ret,buffer = cv2.imencode(".bmp", image)
+            settings['bmp_image_buffer'] = io.BytesIO(buffer)
+            settings['bmp_image_buffer'].flush()
+            settings['bmp_image_buffer'].seek(settings['header_offset'])
+            settings['bmp_image_buffer'] = settings['bmp_image_buffer'].read()
+            
+            if settings['save_img_bin_dump']:
+                with open('img_dump', 'w') as file:
+                    file.write(str(settings['bmp_image_buffer']))
+            #print(time.strftime("[%H:%M:%S]") + "img captured") # Debug line
+            settings['global_lock'].release()
         
-        ret,image = cv2.threshold(image, 230, 255, cv2.THRESH_BINARY)
-        if settings['save_images']: #save image if enabled
-            cv2.imwrite(settings['img_out'],image)
-
-        ret,buffer = cv2.imencode(".bmp", image)
-        settings['bmp_image_buffer'] = io.BytesIO(buffer)
-        settings['bmp_image_buffer'].flush()
-        settings['bmp_image_buffer'].seek(settings['header_offset'])
-        settings['bmp_image_buffer'] = settings['bmp_image_buffer'].read()
-        
-        if settings['save_img_bin_dump']:
-            with open('img_dump', 'w') as file:
-                file.write(str(settings['bmp_image_buffer']))
-        settings['global_lock'].release()
-        #print(time.strftime("[%H:%M:%S]") + "img captured") # Debug line
         time.sleep(settings['image_ref_rate'])
 
-def print_variables():
-    global settings
+def print_variables(settings):
     print(f"\
 settings[is_running] = {settings['is_running']}\n\
 settings[alarm_repeat] = {settings['alarm_repeat']}\n\
@@ -135,12 +154,11 @@ s - start\stop script\n\
 w - change eve window\n\
 u - print usage\n\
 v - print settings\n\
-r[xx] - set alarm repeat (0 - repeat off)\n\
+r [xx] - set alarm repeat (0 - repeat off)\n\
 E - exit script\n\
 ")
 
-def command_processing():
-    global settings
+def command_processing(settings):
     print(time.strftime("[%H:%M:%S]") + "Third eye online.")
     print_usage()
     while True:
@@ -157,20 +175,29 @@ def command_processing():
         elif cmd[0] == 'u':
             print_usage()
         elif cmd[0] == 'v':
-            print_variables()
+            print_variables(settings)
         elif cmd[0] == 'E':
             os._exit(0)
         elif cmd[0] == 'w':
-            select_eve_window()
+            select_eve_window(settings)
+        elif cmd[0] == 'r':
+            try:
+                cmd_val = cmd.split("r ")[1]
+                cmd_val = int(cmd_val)
+                assert cmd_val <= 99 and cmd_val >= 0
+            except:
+                print("wrong alarm repeat value")
+            else:
+                settings['alarm_repeat_time'] = cmd_val
+                print(f"new alarm repeat value is : {settings['alarm_repeat_time']}")
         else:
             print("Unknown cmd")
 
-def get_eve_win_list( hwnd, win_list ):
+def get_eve_win_list(hwnd, win_list):
     if ('EVE -' in win32gui.GetWindowText(hwnd)) and (hwnd not in win_list):
         win_list.append(hwnd)
 
-def select_eve_window():
-    global settings
+def select_eve_window(settings):
     eve_win_list = list()
     settings['global_lock'].acquire()
     while True:
@@ -184,24 +211,47 @@ def select_eve_window():
             settings['global_lock'].release()
             return
 
+def highlight_scr_zone(settings, screenshot_params):
+    win_dc = win32gui.GetDC(settings['eve_win_hwnd'])
+    while True:
+        #draw rect bit larger than actual size 
+        #to prevent highlight frame captured in screen
+        win32gui.DrawFocusRect(win_dc,\
+        (screenshot_params['scr_x1']-1, \
+        screenshot_params['scr_y1']-1, \
+        screenshot_params['scr_x2']+1, \
+        screenshot_params['scr_y2']+1))
+        time.sleep(0.3)
+
+
 
 ###############-MAIN-###############
 
 #init threads
 threads = {
-'cn' : threading.Thread(target=check_neutrals),
-'cb' : threading.Thread(target=check_local_boost),
-'ic' : threading.Thread(target=image_capture),
-'cp' : threading.Thread(target=command_processing)
+'cn' : threading.Thread(target=check_neutrals, args=(settings, )),
+'cb' : threading.Thread(target=check_local_boost, args=(settings, )),
+'ic' : threading.Thread(target=image_capture, args=(settings, )),
+'cp' : threading.Thread(target=command_processing, args=(settings, )),
+'hz' : threading.Thread(target=highlight_scr_zone, args=(settings, screenshot_params))
 }
 
-select_eve_window()
+
+#temp value
+screenshot_params['scr_x1'] = 692
+screenshot_params['scr_x2'] = 702
+screenshot_params['scr_y1'] = 670
+screenshot_params['scr_y2'] = 1050
+
+select_eve_window(settings)
 
 #start threads
 threads['cn'].start()
 threads['cb'].start()
 threads['ic'].start()
 threads['cp'].start()
+#threads['hz'].start()
 
+#do nothing in main cycle, let threads have fun.
 while True:
     time.sleep(1)
